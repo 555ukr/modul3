43e83748667d6300d085bddad78e09b138f6a583
555ukr <kalashnykdmytro@ukr.net>
2019-02-15 21:09:52 +0200

    personal

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ from flask import Flask, request
 from blockchain import Blockchain
 from tinydb import TinyDB, Query
 from flask import Response
+from hashlib import sha256
+import binascii
 
 app = Flask(__name__)
 
@@ -36,4 +38,45 @@ def difficulty():
         status=200,
         mimetype='application/json'
     )
+    return response
+
+
+@app.route('/getBlockByHeight', methods=['POST'])
+def blockReturn():
+    heigh = int(request.form['data'])
+    db = TinyDB('db/blk.json')
+    lst = db.all()[heigh]
+    print(lst)
+    response = app.response_class(
+        response=json.dumps(lst),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+@app.route('/getBlockByHash', methods=['POST'])
+def blockReturnHash():
+    hash= request.form['data']
+    db = TinyDB('db/blk.json')
+    lst = db.all()
+
+    response = app.response_class(
+        response=json.dumps({"status":"not find"}),
+        status=404,
+        mimetype='application/json'
+        )
+    for i in lst:
+        data = sha256(((i["Block Header"]['Version']).to_bytes(4, "little") +
+            binascii.unhexlify(i["Block Header"]['Previous Block Hash']) +
+            binascii.unhexlify(i["Block Header"]['Merkle Root']) +
+            (i["Block Header"]['Timestamp']).to_bytes(4, "little") +
+            (binascii.unhexlify(i["Block Header"]["Difficulty Target"])[::-1]) +
+            (i["Block Header"]['Nonce']).to_bytes(4, "little"))).hexdigest()
+        if data == hash:
+            response = app.response_class(
+                response=json.dumps(i),
+                status=200,
+                mimetype='application/json'
+                )
+            break;
     return response
